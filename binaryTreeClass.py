760056ba65eb19d7d755dffaabff607f789ab514
f3llo <floris-groot_wassink@outlook.com>
2024-12-16 10:05:03 +0100

    yeah

---
+++ binaryTreeClass.py
@@ -0,0 +1,13 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.a = None
+        self.b = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = root
+
+    def add(self, prev_node, node, pos):
+        prev_node.pos = node
